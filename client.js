6095a65aafbda9183030781a5931c388046857c8
CodingNoye <codingnoye@kookmin.ac.kr>
2020-06-18 01:19:16 +0900

    RSA 시드 교환 완성

--- client.js
+++ client.js
@@ -3,13 +3,17 @@ const readline = require('readline')
 const { StreamCipher } = require('./crypt')
 readline.emitKeypressEvents(process.stdin)
 process.stdin.setRawMode(true)
+const fs = require('fs')
+const NodeRSA = require('node-rsa')
+const rsa = new NodeRSA(fs.readFileSync('./public_key.pem'))
 
 const seeds = [1231, 32123, 4124, 123123]
 const key_client = new StreamCipher(seeds)
 
 socket.on('connect', () => {
     console.log('연결됨')
-    socket.emit('seeds', seeds)
+    const seedsSecure = rsa.encrypt(JSON.stringify(seeds))
+    socket.emit('seeds', seedsSecure)
     socket.on('ready', (data) => {
         console.log('연결 완료')
         process.stdin.on('keypress', (str, key) => {
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const { StreamCipher } = require('./crypt')
-
 const io = require('socket.io')(3303)
+const fs = require('fs')
+const NodeRSA = require('node-rsa')
+const rsa = new NodeRSA(fs.readFileSync('./private_key.pem'))
 
 io.sockets.on('connection', (socket) => {
     console.log('연결됨')
     socket.on('seeds', (data) => {
-        const key = new StreamCipher(data)
+        console.log('암호화된 seeds: '+data)
+        const seeds = JSON.parse(rsa.decrypt(data))
+        console.log('복호화된 seeds: '+seeds)
+        const key = new StreamCipher(seeds)
         socket.emit('ready')
         socket.on('text', (data) => {
             process.stdout.write(key.decrypt(data))
